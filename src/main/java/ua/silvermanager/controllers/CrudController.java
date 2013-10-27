da384eb9114909588d8474631ed9f47b803b6861
artem <artem@192.168.1.143>
2013-10-27 09:46:51 +0200

    Nothing changed

--- src/main/java/ua/silvermanager/controllers/CrudController.java
+++ src/main/java/ua/silvermanager/controllers/CrudController.java
@@ -6,9 +6,7 @@
 package ua.silvermanager.controllers;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -22,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 import ua.silvermanager.dao.AdressesDao;
-import ua.silvermanager.dao.ClientsDao;
 import ua.silvermanager.dao.CrudDao;
 import ua.silvermanager.dao.ManagersDao;
 import ua.silvermanager.dao.ServicesDao;
